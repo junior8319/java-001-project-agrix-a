71d110278076f6b004678bb65ee3a41389bf513b
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-09-14 17:31:15 -0300

    feat-Req1-controller-FarmController with post request

---
+++ src/main/java/com/betrybe/agrix/controller/FarmController.java
@@ -0,0 +1,43 @@
+package com.betrybe.agrix.controller;
+
+import com.betrybe.agrix.controller.dto.FarmCreationDto;
+import com.betrybe.agrix.controller.dto.FarmDto;
+import com.betrybe.agrix.service.FarmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The type Farm controller.
+ */
+@RestController
+@RequestMapping("/farms")
+public class FarmController {
+  private final FarmService service;
+
+  /**
+   * Instantiates a new Farm controller.
+   *
+   * @param service the service
+   */
+  @Autowired
+  public FarmController(FarmService service) {
+    this.service = service;
+  }
+
+  /**
+   * Create farm farm dto.
+   *
+   * @param farmCreationDto the farm creation dto
+   * @return the farm dto
+   */
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public FarmDto createFarm(@RequestBody FarmCreationDto farmCreationDto) {
+    return FarmDto.fromEntity(service.create(farmCreationDto.toEntity()));
+  }
+}
