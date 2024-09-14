6d2b3d4f16872bea744e8fedf4e294d2b1fdee27
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-09-14 16:20:07 -0300

    feat-Req1(part)-Entity Farm

---
+++ src/main/java/com/betrybe/agrix/entity/Farm.java
@@ -0,0 +1,46 @@
+package com.betrybe.agrix.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "farms")
+public class Farm {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private Double size;
+
+  public Farm() {}
+
+  public Farm(String name, Double size) {
+    this.name = name;
+    this.size = size;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Double getSize() {
+    return size;
+  }
+
+  public void setSize(Double size) {
+    this.size = size;
+  }
+}
