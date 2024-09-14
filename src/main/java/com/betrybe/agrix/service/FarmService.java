bde06ca2521fb5c5b8c068599c3c06ab80f2e5a8
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-09-14 16:59:19 -0300

    fix-Req1(part)-fix missed dependecy injection annotation

--- src/main/java/com/betrybe/agrix/service/FarmService.java
+++ src/main/java/com/betrybe/agrix/service/FarmService.java
@@ -4,6 +4,7 @@ import com.betrybe.agrix.entity.Farm;
 import com.betrybe.agrix.repository.FarmRepository;
 import com.betrybe.agrix.service.exception.FarmNotFoundException;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,6 +19,7 @@ public class FarmService {
    *
    * @param farmRepository the farm repository
    */
+  @Autowired
   public FarmService(FarmRepository farmRepository) {
     this.farmRepository = farmRepository;
   }
