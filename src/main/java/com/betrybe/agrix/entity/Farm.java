fb384437b59d63b6641f62aa0a494be25cecb4db
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-09-15 07:47:28 -0300

    feat-Req4(part)-entity-set the relationship between Crop(n) and Farm(1)

--- src/main/java/com/betrybe/agrix/entity/Farm.java
+++ src/main/java/com/betrybe/agrix/entity/Farm.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 
 /**
  * The type Farm.
@@ -19,6 +21,9 @@ public class Farm {
   private String name;
 
   private Double size;
+
+  @OneToMany(mappedBy = "crop")
+  private List<Crop> crops;
 
   /**
    * Instantiates a new Farm.
@@ -79,5 +84,23 @@ public class Farm {
    */
   public void setSize(Double size) {
     this.size = size;
+  }
+
+  /**
+   * Gets crops.
+   *
+   * @return the crops
+   */
+  public List<Crop> getCrops() {
+    return crops;
+  }
+
+  /**
+   * Sets crops.
+   *
+   * @param crops the crops
+   */
+  public void setCrops(List<Crop> crops) {
+    this.crops = crops;
   }
 }
